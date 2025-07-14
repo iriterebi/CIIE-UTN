2f850e27783147726c84517731dc6ff894520433
Jesus Figueredo <jfigueredochirinos@frba.utn.edu.ar>
2025-07-14 18:06:35 -0300

    WIP Docker DB improvements

--- Db/def/migrations/20250714193959_initial.sql
+++ Db/def/migrations/20250714193959_initial.sql
@@ -1,17 +1,17 @@
-CREATE DATABASE IF NOT EXISTS ciie_db;
+-- migrate:up
 
-drop table if EXISTS ciie_db.ciie_table;
-
-CREATE TABLE ciie_db.ciie_table (
+CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombreCompleto VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     usr_name VARCHAR(255) NOT NULL UNIQUE,
-    usr_psw VARCHAR(255) NOT NULL, -- TODO guarda en md5 y en otra tabla
+    usr_psw VARCHAR(255) NOT NULL, -- TODO guarda en bcrypt o Argon2
     statuss ENUM('profe', 'alumno'),
     usr_pronouns VARCHAR(255),
     Accion ENUM('Girar', 'Pinzar', 'Reverencia') -- Se asume que 'Girar' es el valor por defecto deseado
 );
 
-CREATE UNIQUE INDEX idx_ciie_table_usr_name
-    ON ciie_db.ciie_table (usr_name);
+-- migrate:down
+
+DROP TABLE IF EXISTS usuarios;
+
