2edf281ddb89b545cbe8f62fce60683cb20de912
Irina <irina@inorbit.ai>
2026-03-24 11:14:24 -0300

    systemctl

--- ros_tryouts/2026/tmux-start.sh
+++ ros_tryouts/2026/tmux-start.sh
@@ -5,6 +5,6 @@ SESSION="ros_agent_session"
 tmux new-session -d -s $SESSION
 
 tmux rename-window -t $SESSION:0 "InOrbit"
-tmux send-keys -t $SESSION:0 "export ROS_DOMAIN_ID=42 && curl https://control.inorbit.ai/liftoff/WklSDRtViJNzWRPN | sh" Enter
+tmux send-keys -t $SESSION:0 "/root/.inorbit/dist/inorbit_ros" Enter
 
 tmux attach-session -t $SESSION
