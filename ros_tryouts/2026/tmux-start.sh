471f8cbf009c54a432bd271bbf238f49ff90ab02
Irina <irina@inorbit.ai>
2026-03-24 11:35:02 -0300

    fixed docker?

--- ros_tryouts/2026/tmux-start.sh
+++ ros_tryouts/2026/tmux-start.sh
@@ -5,6 +5,6 @@ SESSION="ros_agent_session"
 tmux new-session -d -s $SESSION
 
 tmux rename-window -t $SESSION:0 "InOrbit"
-tmux send-keys -t $SESSION:0 "/root/.inorbit/dist/inorbit_ros" Enter
+tmux send-keys -t $SESSION:0 "source /opt/ros/humble/setup.bash && /root/.inorbit/dist/scripts/start.sh" Enter
 
 tmux attach-session -t $SESSION
