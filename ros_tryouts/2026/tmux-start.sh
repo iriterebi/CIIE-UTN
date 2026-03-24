858ff7bfb40fd68b020744cc121146fefddbc4b2
Irina <irina@inorbit.ai>
2026-03-24 11:07:10 -0300

    ..

--- ros_tryouts/2026/tmux-start.sh
+++ ros_tryouts/2026/tmux-start.sh
@@ -5,7 +5,7 @@ SESSION="ros_agent_session"
 tmux new-session -d -s $SESSION
 
 tmux rename-window -t $SESSION:0 "InOrbit"
-tmux send-keys -t $SESSION:0 "export ROS_DOMAIN_ID=0 && curl https://control.inorbit.ai/liftoff/WklSDRtViJNzWRPN | sh" Enter
+tmux send-keys -t $SESSION:0 "export ROS_DOMAIN_ID=42 && curl https://control.inorbit.ai/liftoff/WklSDRtViJNzWRPN | sh" Enter
 
 tmux new-window -t $SESSION:1 -n "Extra tab"
 
